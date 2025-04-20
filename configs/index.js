91bc22b2b71ae5b015471caa6b154f7768781923
Your GitHub Username <your-github-email@example.com>
2025-04-20 21:13:32 +0530

    ""

--- configs/index.js
+++ configs/index.js
@@ -1,8 +1,8 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from "./schema"
 
 const sql = neon(import.meta.env.VITE_DATABASE_URL);
-import * as schema from "./schema"
 export const db = drizzle({ client: sql },schema);
 
 // const result = await db.execute('select 1');
