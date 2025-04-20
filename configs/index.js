d5e22bd1ae979ede6b7eac08495f34cab89c2b7c
Devbandil12 <devbandil120@gmail.com>
2025-04-20 21:30:08 +0530

    Initial commit

--- configs/index.js
+++ configs/index.js
@@ -4,4 +4,4 @@ import * as schema from "./schema"
 
 const sql = neon(import.meta.env.VITE_DATABASE_URL);
 export const db = drizzle({ client: sql },schema);
-// const result = aait db.execute('select 1');
+// const result = await db.execute('select 1');
