1d14deda92a2ba914597916d9e07d2a5f1213d9e
omkarkaurav <omkarkaurav41@gmail.com>
2025-07-26 13:00:56 +0530

    Update ProductDetail.jsx

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -92,7 +92,7 @@ const ProductDetail = ({
         {/* Right Product Section */}
         <div className="w-full md:w-1/2 p-6 flex flex-col justify-between relative">
           <button onClick={onClose} className="absolute right-4 top-4 text-2xl text-gray-500 hover:text-gray-800 font-bold">×</button>
-
+</div>
           <div className="w-full flex items-center justify-between mt-4 px-2">
   <h2 className="text-sm font-semibold text-gray-800">{product.name}</h2>
   <button onClick={onToggleWishlist} className="hover:scale-110 transition">
