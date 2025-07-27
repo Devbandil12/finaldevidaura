dae7d8870a3d9b61d917cef37fab9ecec6105de1
omkarkaurav <omkarkaurav41@gmail.com>
2025-07-27 11:52:09 +0530

    Update HeroSection.jsx

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
           </button>
 
           {/* Sign In button redirects to auth page */}
-          <button className="hero__signin" onClick={() => navigate("/auth")}>
+          <button className="hero__signin" onClick={() => navigate("/Login")}>
             Sign In
           </button>
         </div>
