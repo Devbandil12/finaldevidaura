2367508de3078abc6a4763fbf799f6577fbd35c6
omkarkaurav <omkarkaurav41@gmail.com>
2025-08-06 02:02:17 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -452,7 +452,7 @@ useEffect(() => {
                       <img src={UserIcon} alt="User" />
                       {isLoggedIn ? (
                         <div className="sidebar-user">
-                          <h4>{user?.fullName}</h4>
+                          <h4>{user?.name}</h4>
                           <p>{user?.primaryEmailAddress?.emailAddress || "N/A"}</p>
                         </div>
                       ) : (
