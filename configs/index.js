cabc186459ecfd7b858c23c4adc53688b431621a
omkarkaurav <omkarkaurav41@gmail.com>
2025-08-24 15:12:15 +0530

    Update index.js

--- configs/index.js
+++
@@ -1,8 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from "./schema"
-
-const sql = neon(import.meta.env.VITE_DATABASE_URL);
-export const db = drizzle({ client: sql },schema);
-console.log("hello")
-// const result = await db.execute('select 1');
