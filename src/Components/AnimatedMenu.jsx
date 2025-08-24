098648dfa45f4c4713c0345ee77eeafd303a5bf8
omkarkaurav <omkarkaurav41@gmail.com>
2025-08-24 19:29:26 +0530

    Update AnimatedMenu.jsx

--- src/Components/AnimatedMenu.jsx
+++ src/Components/AnimatedMenu.jsx
@@ -6,15 +6,15 @@ const AnimatedMenu = ({ size = 50 }) => {
 
   return (
     <div className="menu-nav" style={{ width: size, height: size }}>
-      <svg
-        className={`menu-icon ${open ? "open" : ""}`}
-        viewBox="0 0 100 56"
-        onClick={() => setOpen((prev) => !prev)}
-      >
-        <path
-          d="M48.33,45.6H18a14.17,14.17,0,0,1,0-28.34H78.86a17.37,17.37,0,0,1,0,34.74H42.33l-21-21.26L47.75,4"
-        />
-      </svg>
+     <svg
+  className={`menu-icon ${open ? "open" : ""}`}
+  viewBox="0 0 100 56"
+  onClick={() => setOpen((prev) => !prev)}
+>
+  <path d="M48.33,45.6H18a14.17,14.17,0,0,1,0-28.34H78.86a17.37,17.37,0,0,1,0,34.74H42.33l-21-21.26L47.75,4"/>
+  <path d="M48.33,45.6H18a14.17,14.17,0,0,1,0-28.34H78.86a17.37,17.37,0,0,1,0,34.74H42.33l-21-21.26L47.75,4"/>
+</svg>
+
     </div>
   );
 };
