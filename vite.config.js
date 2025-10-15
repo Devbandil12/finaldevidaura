185592e4b2ca79e6b528bfcfed7ecd42776bb5cd
omkarkaurav <omkarkaurav41@gmail.com>
2025-10-15 09:25:38 +0530

    path fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // The key change to fix the refresh issue is correctly implemented here
-  base: './',
+  base: '/',
   
   plugins: [
     react(),
